9b4fada5759b78d2815d3ac5c7f289e89184a6ce
obaye <hi.savis@gmail.com>
2019-10-01 14:33:06 +0100

    Add ShowEvents.js in events directory of src

---
+++ client/src/components/events/ShowEvents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Owners from '../users/owners/Owners';
+
+ 
+const ShowEvents = () => {
+  return (
+      <div>
+        events
+        {/* <Owners /> */}
+      </div>
+  );
+}
+
+export default ShowEvents;
