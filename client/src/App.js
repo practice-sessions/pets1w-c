46da92a4da253082099427c3a6443d4145cb0e1b
obaye <hi.savis@gmail.com>
2020-01-29 12:23:09 +0000

    Update frontend dependencies+devdependencies, App.css, App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,50 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Categories from '../src/categories/Categories';
 import ShowEvents from '../src/components/events/ShowEvents';
+import RegUserClient from './components/users/auth/authUserForms/RegUserClient';
+import LoginUserClient from './components/users/auth/authUserForms/LoginUserClient'; 
+import Alerts from '../src/components/navbar/Alerts';
+import PrivateRoute from '../src/components/routing/PrivateRoute';
+import ShowNotFound from '../src/components/events/ShowNotFound'; 
+
+import OwnBioState from './context/ownbio/OwnBioState';
+import AuthState from './context/auth/AuthState';
+import PetState from './context/pet/PetState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from '../src/utils/setAuthToken';
 import './App.css';
 
-const App = () => {
-  return (
-    <Router>
-      <Fragment>
-        <Categories>
-          <div className="container">
-            <Switch>
-              <Route exact path='/welcome' component={ShowEvents} />
-              {/* <Route exact path='/add-petbio' component={AddPetBioClient} /> */}
-              {/*<Route exact path='/get-petbio' component={GetOwnerPetBio} />*/}
-            </Switch>
-          </div>
-        </Categories>
-      </Fragment>
-    </Router>
-  );
-};
+// To get token from local storage
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
+const App = () => {
+
+  return (
+    <AuthState>
+      <OwnBioState>
+        <PetState>
+          <AlertState>
+            <Router>
+              <Fragment>
+                <Categories>
+                  <div className="container">
+                    <Alerts />
+                    <Switch>
+                      <PrivateRoute exact path='/welcome' component={ShowEvents} />
+                      <Route exact path='/register' component={RegUserClient} />
+                      <Route exact path='/get-client' component={LoginUserClient} />
+                      <Route component={ShowNotFound} />
+                    </Switch>
+                  </div>
+                </Categories>
+              </Fragment>
+            </Router>
+          </AlertState>
+        </PetState>
+      </OwnBioState>
+    </AuthState>
+  );
+}
 export default App;
